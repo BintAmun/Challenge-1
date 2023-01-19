60aebfaeca4f9b14bdbfebac9f79d73772ca7018
Ashley <64813439+BintAmun@users.noreply.github.com>
2023-01-19 13:09:57 -0500

    Printing Letters

---
+++ YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("  A  " + "  " + "BBBBB");
+        System.out.println(" A A " + "  " + "B   B");
+        System.out.println("A   A" + "  " + "B   B");
+        System.out.println("AAAAA" + "  " + "BBBB");
+        System.out.println("A   A" + "  " + "B   B" );
+        System.out.println("A   A" + "  " +  "B   B");
+        System.out.println("A   A" + "  " + "BBBB ");
+
+    }
+}
